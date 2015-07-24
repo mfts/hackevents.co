160df8fc8c7a88b5bd8aac61d849b76c6ba57f55
Marc Ft <marcftone@gmail.com>
2015-07-24 12:58:52 +0200

    Add categories model and controller and association with hackathon

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724095957) do
+ActiveRecord::Schema.define(version: 20150724103838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.integer  "category_id"
+    t.integer  "hackathon_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "hackathons", force: :cascade do |t|
     t.string   "title"
