abec49c52ec4304e84730cdf10096c5585bddf41
Devran Ünal <devran@me.com>
2015-07-26 17:17:21 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725233901) do
+ActiveRecord::Schema.define(version: 20150726115833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "primary_color"
   end
 
   create_table "categorizations", force: :cascade do |t|
