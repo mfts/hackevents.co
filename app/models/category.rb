701c05bb37db026a4a44abb2da3f1aef51e40354
Devran Ünal <devran@me.com>
2015-07-26 17:17:36 +0200

    Add initial views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :hackathons, :through => :categorizations
-
+  
+  def primary_hex_color
+    self.primary_color.to_s(16).rjust(6, "0")
+  end
 end
