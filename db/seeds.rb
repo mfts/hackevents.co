85a50eedbfff341130ab0193fb08fa10a097277e
Devran Ünal <devran@me.com>
2015-07-27 22:28:23 +0200

    Turn create to first_or_create (to allow us rerun the seeds file)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create(name: "Finance",                  primary_color: 5789398)   #5856D6
-Category.create(name: "Science & Education",      primary_color: 31487)     #007AFF
-Category.create(name: "Technology",               primary_color: 3451612)   #34AADC
-Category.create(name: "Open data / source",       primary_color: 5949690)   #5AC8FA
-Category.create(name: "Travel & Transportation",  primary_color: 5036388)   #4CD964
-Category.create(name: "Fun & Entertainment",      primary_color: 16763904)  #FFCC00
-Category.create(name: "Health & Fitness",         primary_color: 16749824)  #FF9500
-Category.create(name: "Without Topic",            primary_color: 16726832)  #FF3B30
-Category.create(name: "Specific Interest",        primary_color: 16723285)  #FF2D55
-Category.create(name: "Unknown",                  primary_color: 9342611)   #8E8E93
+Category.where(name: "Finance",                  primary_color: 5789398).first_or_create   #5856D6
+Category.where(name: "Science & Education",      primary_color: 31487).first_or_create     #007AFF
+Category.where(name: "Technology",               primary_color: 3451612).first_or_create   #34AADC
+Category.where(name: "Open data / source",       primary_color: 5949690).first_or_create   #5AC8FA
+Category.where(name: "Travel & Transportation",  primary_color: 5036388).first_or_create   #4CD964
+Category.where(name: "Fun & Entertainment",      primary_color: 16763904).first_or_create  #FFCC00
+Category.where(name: "Health & Fitness",         primary_color: 16749824).first_or_create  #FF9500
+Category.where(name: "Without Topic",            primary_color: 16726832).first_or_create  #FF3B30
+Category.where(name: "Specific Interest",        primary_color: 16723285).first_or_create  #FF2D55
+Category.where(name: "Unknown",                  primary_color: 9342611).first_or_create   #8E8E93
+
+
 
