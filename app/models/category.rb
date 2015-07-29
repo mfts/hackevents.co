02028c405c652a1a3149e28bda6c8edd015fb93b
Devran Ünal <devran@me.com>
2015-07-30 01:08:02 +0200

    Add custom setter for hex colors

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,8 @@ class Category < ActiveRecord::Base
   def primary_hex_color
     self.primary_color.to_s(16).rjust(6, "0")
   end
+  
+  def primary_hex_color=(value)
+    self.primary_color = value.to_i(16)
+  end
 end
