93729bc5e8893075171584e3ab19e241583247cb
Devran Ünal <devran@me.com>
2015-07-30 04:22:29 +0200

    Add admin field for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726120335) do
+ActiveRecord::Schema.define(version: 20150730021834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20150726120335) do
     t.string   "password_reset_token"
     t.boolean  "email_confirmed",      default: false
     t.string   "confirm_token"
+    t.boolean  "admin",                default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
