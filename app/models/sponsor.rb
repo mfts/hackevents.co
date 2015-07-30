53d2e0c8e7bedd35c1f15cd61a1876affbcbd9cf
Devran Ünal <devran@me.com>
2015-07-30 03:45:52 +0200

    Add virtual name attribute

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,4 +1,8 @@
 class Sponsor < ActiveRecord::Base
   has_many :sponsorships
   has_many :hackathons, :through => :sponsorships
+  
+  def name
+    [self.alt_name, self.twitter_name, self.twitter_handle].find { |n| n.present? }
+  end
 end
