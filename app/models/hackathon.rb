2a4ed028075f5f8d66dd559f38bda52ed2eea140
Devran Ünal <devran@me.com>
2015-08-04 00:30:18 +0200

    Query hackathon by day names

--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -10,7 +10,13 @@ class Hackathon < ActiveRecord::Base
 
   has_many :sponsorships
   has_many :sponsors, :through => :sponsorships, dependent: :destroy
-
+  
+  before_save :update_days_mask
+  
+  scope :with_days, lambda { |days| where("(days_mask & ?) > 0", days.map { |d| 2**DAYS.index(d) }.sum) }
+  
+  DAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+  
   def uniqueslug
     "#{country} #{city} #{title}"
   end
@@ -21,7 +27,17 @@ class Hackathon < ActiveRecord::Base
       Hackathon.create! row.to_hash
     end
   end
-
+  
+  def update_days_mask
+    self.days_mask = possible_days.map { |d| 2**DAYS.index(d) }.sum
+  end
+  
+  def possible_days
+    ((date_start.to_date)..(date_start.to_date)).to_a.map do |t|
+      t.strftime("%A")
+    end.uniq
+  end
+  
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731230936) do
+ActiveRecord::Schema.define(version: 20150802211408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150731230936) do
     t.string   "twitter"
     t.string   "image_url"
     t.boolean  "highlighted",          default: false
+    t.integer  "days_mask"
   end
 
   add_index "hackathons", ["slug"], name: "index_hackathons_on_slug", using: :btree
