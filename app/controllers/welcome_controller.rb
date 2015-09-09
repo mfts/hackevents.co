217143ed97c5a5d5348297c3f8fbc2efdd56c2e7
Marc Ft <marcftone@gmail.com>
2015-09-10 01:48:07 +0200

    Make hackathon count live

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,8 @@ class WelcomeController < ApplicationController
 
   def index
     @hackathons = @q.result.where('date_start >= ?', Time.zone.now).order(date_start: :asc).page(@page).per(@per_page)
-    @featured = @hackathons.where(highlighted: true)
+    @featured = @hackathons.where('date_start >= ? AND highlighted = ?', Time.zone.now, true)
+    @count = Hackathon.where('date_start >= ?', Time.now).count
     @result = request.location
   end
 end
