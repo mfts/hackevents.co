cb3e60f9ccef59d517570426226a73c840e30ccd
Marc Ft <marcftone@gmail.com>
2015-09-10 03:19:03 +0200

    Clean welcome controller and add logic to view regarding ip_address

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,9 @@ class WelcomeController < ApplicationController
   layout "layouts/home"
 
   def index
-    @hackathons = @q.result.where('date_start >= ?', Time.zone.now).order(date_start: :asc).page(@page).per(@per_page)
-    @featured = @hackathons.where('date_start >= ? AND highlighted = ?', Time.zone.now, true)
-    @count = Hackathon.where('date_start >= ?', Time.now).count
     @result = request.location
+    @featured = Hackathon.where('date_start >= ? AND highlighted = ?', Time.zone.now, true)
+    @count = Hackathon.where('date_start >= ?', Time.now).count
     @cities = City.order(hackathon_count: :desc)
     @city_count = City.count
   end
