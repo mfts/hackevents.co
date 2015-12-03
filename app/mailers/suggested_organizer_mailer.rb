e1f40d669e8c8aadd0764911ba1fd6ec857e28be
Marc Ft <marcftone@gmail.com>
2015-12-03 17:33:43 +0100

    fix actionmailer for suggested organizer

--- app/mailers/suggested_organizer_mailer.rb
+++ app/mailers/suggested_organizer_mailer.rb
@@ -1,11 +1,11 @@
 class SuggestedOrganizerMailer < ApplicationMailer
-  default from: "Hackevents <dev@hackevents.co>"
+  default from: "Hackevents <info@hackevents.co>"
   
   def suggestions(user,hackathon,input)
     @user = user
     @hackathon = hackathon
     @input = input
-    mail(:to => "dev@hackevents.co, michael@hackevents.co", 
+    mail(:to => "<michael@hackevents.co>, <dev@hackevents.co>", 
          :subject => "New Suggested Organizer")
   end
 end
